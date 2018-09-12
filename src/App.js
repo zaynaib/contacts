b27fd2f730c3342db66308906e0a9ab4ce22d733
Zaynaib Giwa <zaynaibg@gmail.com>
2018-09-12 12:30:03 -0500

    put people data into the props of ContactList being rendered

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 
 class ContactList extends React.Component {
     render(){
-      const people = [
-        {name:'Michael'},
-        {name: 'Ryan'},
-        {name:'Tyler'}
-      ]
+      const people =this.props.contacts
       return <ol>
         {people.map(person => (<li key={person.name}>{person.name}</li>
       ))}
@@ -20,7 +16,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ContactList/>
+        <ContactList contacts={[
+          {name:'Michael'},
+          {name:'Ryan'},
+          {name:'Tyler'}
+          ]}/>
+        <ContactList contacts={[
+          {name:'Amanda'},
+          {name:'Richard'},
+          {name:'Jeff'}
+        ]}/>
       </div>
     );
   }
