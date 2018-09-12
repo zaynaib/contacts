5c38b3e5bc18222100abfe296ac5497f206f51c0
Zaynaib Giwa <zaynaibg@gmail.com>
2018-09-12 11:49:43 -0500

    created a react app which displays an ordered list of contacts

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class ContactList extends React.Component {
+    render(){
+      const people = [
+        {name:'Michael'},
+        {name: 'Ryan'},
+        {name:'Tyler'}
+      ]
+      return <ol>
+        {people.map(person => (<li key={person.name}>{person.name}</li>
+      ))}
+      </ol>
+    }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContactList/>
+      </div>
+    );
+  }
+}
+
+export default App;
